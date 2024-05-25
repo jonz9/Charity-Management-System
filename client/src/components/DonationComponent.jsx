8db4fac121d508740b34f3d541fdd8470e7e3e32
[Max <Max.tyu.ju@gmail.com>
2024-05-25 14:53:52 -0400

    change donation link button

--- client/src/components/DonationComponent.jsx
+++ client/src/components/DonationComponent.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function DonationComponent() {
   return (
-    <button type="submit" className="bg-black text-white border border-red-400 py-2 px-4 rounded">
+    <button type="submit" className="bg-blue-200 text-black border border-blue-500 py-4 px-8 rounded hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
       <a href="/CharityPage">Donate NOW!</a>
     </button>
   );
