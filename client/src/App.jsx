4850558821a55f4040f05821014d2af4d6cd217f
j444zhan <j444zhan@uwaterloo.ca>
2024-05-25 00:43:59 -0400

    router from HomePage to CharityPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import './index.css'
+import "./index.css";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <div>
-      <h1 className="underline font-bold">Hello World</h1>
+      <HomePage />
     </div>
   );
 };
