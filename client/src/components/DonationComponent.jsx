d4f67400b44df09c8b3e9666355c869977efc1ce
[Max <Max.tyu.ju@gmail.com>
2024-05-25 16:57:15 -0400

    committing changes

--- client/src/components/DonationComponent.jsx
+++ client/src/components/DonationComponent.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import '../index.css';
 
 function DonationComponent() {
   return (
     <a href="/CharityPage">
-      <button type="submit" className="bg-blue-200 text-black border border-blue-500 py-4 px-8 rounded hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <button 
+        type="submit" 
+        className="bg-blue-200 text-black border border-blue-500 py-8 px-28 rounded-full hover:bg-blue-700 hover:text-white hover:scale-105 font-playfair font-bold text-6xl"
+        style={{ fontFamily: 'Playfair Display, serif' }}
+      >
         <h1>Donate Today</h1>
       </button>
     </a>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,12 +9,12 @@ function Navbar() {
           <img src={Logo} alt="logo" className="w-50 h-20 m-3 cursor-pointer" />
         </a>
         <div className="grid grid-flow-col gap-2">
-          <button className="p-3 border-2 border-black rounded-xl border-spacing-2 bg-slate-400 hover:bg-slate-500">
+        <button className="p-3 border-2 border-black rounded-xl bg-gradient-to-br from-slate-400 to-slate-500 hover:from-slate-300 hover:to-slate-600">
             <button>
               <h3>Register Charity</h3>
             </button>
           </button>
-          <button className="p-3 border-2 border-black rounded-xl border-spacing-2 bg-slate-400 hover:bg-slate-500">
+          <button className="p-3 border-2 border-black rounded-xl bg-gradient-to-br from-slate-400 to-slate-500 hover:from-slate-300 hover:to-slate-600">
             <h3>
               // verification
             </h3>
